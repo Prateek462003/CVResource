a9bef4924d3fcaba3a7a929cb6cb05e964eab08a
hub_Prateek <prateeksaxena462003@gmail.com>
2024-03-31 01:27:04 +0530

    Update migration.rb

--- migration.rb
+++ migration.rb
@@ -1,17 +1,7 @@
-class UpdateProjectDataWithVersion < ActiveRecord::Migration[7.0]
-  def up
-    ProjectDatum.all.each do |datum|
-      # Parse the existing JSON data
-      data = JSON.parse(datum.data) rescue {}
-
-      # Check if simulatorVersion key doesn't already exists this is to be done for only old circuits 
-      unless data.key?("simulatorVersion")
-        # Assuming old circuits have to be loaded on version1
-        data["simulatorVersion"] = "version1"
-      end
-
-      datum.update!(data: data.to_json)
-    end
+class AddSimulatorVersionToProjects < ActiveRecord::Migration[6.1]
+  def change
+    add_column :projects, :simulatorVersion, :string, default: '1.0', null: false
   end
 end
+
 
