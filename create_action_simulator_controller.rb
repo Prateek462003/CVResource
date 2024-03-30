e226718327cb003751d678bff453a586312301cc
hub_Prateek <prateeksaxena462003@gmail.com>
2024-03-31 01:52:59 +0530

    Update create_action_simulator_controller.rb

--- create_action_simulator_controller.rb
+++ create_action_simulator_controller.rb
@@ -3,8 +3,10 @@
     @project.build_project_datum.data = sanitize_data(@project, params[:data])
     @project.name = sanitize(params[:name])
     @project.author = current_user
-    # Latest will be replaced by the latest version for the simulator
-    simulator_version = "latest"
+
+    # Newly Created circuits will be made on vue simulaltor that will be store in config/application.rb
+    @project.simulaltorVersion = Rails.configuration.simulaltor_version
+    
     # ActiveStorage
     io_image_file = parse_image_data_url(params[:image])
     attach_circuit_preview(io_image_file)
@@ -12,8 +14,6 @@
     image_file = return_image_file(params[:image])
     @project.image_preview = image_file
     image_file.close
-    # Update project data with simulator version
-    @project.build_project_datum.data.merge!({ "simulatorVersion": simulator_version })
     @project.save!
 
     # render plain: simulator_path(@project)
