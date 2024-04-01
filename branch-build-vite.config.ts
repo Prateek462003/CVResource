7c70ec9bd2899fc838e92d6f7c87d06037b353c2
hub_Prateek <prateeksaxena462003@gmail.com>
2024-04-02 02:28:41 +0530

    Create branch-build-vite.config.ts

---
+++ branch-build-vite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, ConfigEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import vuetify from 'vite-plugin-vuetify'
+
+const proxyUrl: string = 'http://localhost:3000'
+
+export default ({ command }: ConfigEnv) => {
+  const version = process.env.VITE_BUILD_VERSION;
+
+  // Checkout specified branch using a child process
+  const checkoutBranch = childProcess.execSync(`git checkout ${version}`);
+
+  // Ensure branch checkout completion for build accuracy
+  if (checkoutBranch.status !== 0) {
+    throw new Error(`Failed to checkout branch: ${version}`);
+  }
+
+  // Root source directory remains constant
+  const entryDir = './src';
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      vuetify({ autoImport: true }),
+      vueI18n({
+        include: fileURLToPath(new URL(entryDir + '/locales/**', import.meta.url)),
+      }),
+    ],
+    resolve: {
+      alias: {
+        '#': fileURLToPath(new URL(entryDir, import.meta.url)),
+        '@': fileURLToPath(new URL(entryDir + '/components', import.meta.url)),
+      },
+    },
+    base: '/simulatorvue/',
+    build: {
+      outDir: `../public/simulatorvue-${version}`,
+      assetsDir: 'assets',
+      chunkSizeWarningLimit: 1600,
+    },
+    server: {
+      port: 4000,
+      proxy: {
+        '^/(?!(simulatorvue)).*': {
+          target: proxyUrl,
+          changeOrigin: true,
+          headers: {
+            origin: proxyUrl,
+          },
+        },
+      },
+    },
+  })
+}
